f03697c845c73b2e53a8d4e784b31e9c676c213c
Rayhan <rayhanderya9@gmail.com>
2025-10-15 00:56:47 +0700

    Notif Success

--- notif/urls.py
+++ notif/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from notif.views import show_main, show_notif, create_notif
+from notif.views import show_main, show_notif, create_notif, register, login_user,logout_user
 
 app_name = 'notif'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-notif/', create_notif, name='create_notif'),
-    path('notif/', show_notif, name='show_notif')
+    path('notif/', show_notif, name='show_notif'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout')
+
 ]
--- notif/views.py
+++ notif/views.py
@@ -3,8 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from notif.models import Notif
 from notif.forms import NotifForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
     context = {
         'test' : 'INI TEST PROYEK.'
@@ -20,10 +25,11 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your account has been successfully created!')
-            return redirect('main:login')
+            return redirect('notif:login')
     context = {'form':form}
     return render(request, 'register.html', context)
 
+@login_required(login_url='/login')
 def show_notif(request):
     list_notif = Notif.objects.all()
     
@@ -36,9 +42,28 @@ def show_notif(request):
 def create_notif(request):
     form = NotifForm(request.POST or None)
     if form.is_valid() and request.method == "POST":
-        form.save()
+        notifikasi = form.save(commit=False)
+        notifikasi.sender = request.user
+        notifikasi.save()
         return redirect('notif:show_main')
     
     context = {'form': form}
     return render(request, "create_notif.html", context)
-    
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('notif:show_main')
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('notif:login')
